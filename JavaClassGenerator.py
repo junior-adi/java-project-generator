396925c6c60dc5ab08a02ea93985a32e7b0bce7a
Junior Adi <81391773+junior-adi@users.noreply.github.com>
2025-01-14 09:06:26 +0000

    Update JavaClassGenerator.py

--- JavaClassGenerator.py
+++ JavaClassGenerator.py
@@ -12,6 +12,19 @@ Version: 1.0
 Date: 2025-01-13
 License: GPLv3 License
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 Usage:
     - Ensure Python 3.x is installed.
     - Install required dependencies: `pip install inflection`.
